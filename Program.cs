c19d42e70e350e4e2020964931f808f740ae445b
Fesposito9197 <kekkoesp8@gmail.com>
2023-03-31 15:47:12 +0200

    aggiunge un nuovo videogame dentro la tabella videogame e una nuova softwarehouse

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using net_ef_videogame;
+
+using (VideogameContext db = new VideogameContext())
+{
+    //Create Software House
+    SoftwareHouse nuovasoftwareHouse = new SoftwareHouse { name = "Ubisoft", city = "Montreal", country = "Canada" };
+    db.Add(nuovasoftwareHouse);
+    db.SaveChanges();
+
+    //Create Videogame
+    Videogame aggiungivideogmae = new Videogame { SoftwareHouseId = 2, name = "call of duty", overview = "prova" };
+    db.Add(aggiungivideogmae);
+    db.SaveChanges();
+}
